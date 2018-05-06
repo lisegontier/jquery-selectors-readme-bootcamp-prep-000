78a0e6769d387a18581b3b3eb2e6f19e1974464d
Liz Gunter <midwestunicorn@gmail.com>
2018-05-06 21:59:37 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -5,17 +5,17 @@ function paragraphSelector() {
 }
 
 function lastImageSelector() {
-  $('img:last');
+  return $('img:last');
 }
 
 function ninjaBabySelector() {
-  $('#baby-ninja');
+  return $('#baby-ninja');
 }
 
 function divSelector() {
-  $('.pics');
+  return $('.pics');
 }
 
 function firstListItem() {
-  $('.pic-list:first child');
+  return $('.pic-list:first child');
 }
